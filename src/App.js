b26d29021c6102c22d55a6b7035b9efcb10997e7
SINETEMBA LAWRENCE NKOSANA <sinetemba.nkosana@mercedes-benz.com>
2023-01-24 17:14:43 +0200

    login and singup forms

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import './App.css'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
+import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
-
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/signup">
+            <Signup />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
